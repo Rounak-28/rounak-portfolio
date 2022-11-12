d16a0e07314af7b0f56c84c5060dbefc8953d190
Rounak-28 <95576871+Rounak-28@users.noreply.github.com>
2022-11-12 22:24:40 +0530

    some improvement

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -7,17 +7,17 @@ const Projects = () => {
       <div className="pt-8">
         <p className="pl-[10%] text-xl text-[#9d94a5]">PROJECTS</p>
         <div className="card flex flex-col items-center">
-          <ProjectCard title="CryptoNews" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Error porro praesentium corporis? Nisi voluptates reprehenderit magnam molestiae dolore, id, qui, ex nesciunt neque tempora error impedit sit porro nihil nobis."
-          img="/cryptonews.jpg" />
+          <ProjectCard title="CryptoNews" description="A website where you can track price of 100 crypto currencies. You can also get recent top news related to crypto."
+          img="/cryptonews.jpg" codeLink="https://github.com/Rounak-28/crypto-price-tracker" liveLink="https://crypto-price-tracker-seven.vercel.app/" />
 
-          <ProjectCard title="Twitter Clone" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Error porro praesentium corporis? Nisi voluptates reprehenderit magnam molestiae dolore, id, qui, ex nesciunt neque tempora error impedit sit porro nihil nobis."
-          img="/twitter.jpg" />
+          <ProjectCard title="Twitter Clone" description="A twitter clone where you can create account/login and post tweets."
+          img="/twitter.jpg" codeLink="https://github.com/Rounak-28/twitter-clone" liveLink="https://rounak-tweet.vercel.app/" />
 
-          <ProjectCard title="SpaceX Clone" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Error porro praesentium corporis? Nisi voluptates reprehenderit magnam molestiae dolore, id, qui, ex nesciunt neque tempora error impedit sit porro nihil nobis."
-          img="/spacex.jpg" />
+          <ProjectCard title="SpaceX Clone" description="A responsive spaceX website clone using ReactJs & TailwindCSS"
+          img="/spacex.jpg" codeLink="https://github.com/Rounak-28/spacex-clone" liveLink="https://rounak-28.github.io/spacex-clone/" />
 
-          <ProjectCard title="Notes App" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Error porro praesentium corporis? Nisi voluptates reprehenderit magnam molestiae dolore, id, qui, ex nesciunt neque tempora error impedit sit porro nihil nobis."
-          img="/notes.jpg" />
+          <ProjectCard title="Notes App" description="A simple app where you can upload text to database(firebase)."
+          img="/notes.jpg" codeLink="https://github.com/Rounak-28/notes-app-firebase" liveLink="https://notes-app-firebase.vercel.app/" />
         </div>
       </div>
     </Element>
